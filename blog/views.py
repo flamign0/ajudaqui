208b4168b7f68063fb26b45995efe43a081eaecc
Ruan <ruanmassini@gmail.com>
2022-09-20 09:35:15 -0400

    Configurei views post_detail  adicionei arquivo post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Anuncio
 
@@ -6,3 +6,7 @@ from .models import Anuncio
 def post_list(request):
     anuncios = Anuncio.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
     return render(request, 'blog/post_list.html', {'anuncios': anuncios})
+
+def post_detail(request, pk):
+    anuncio = get_object_or_404(Anuncio, pk=pk)
+    return render(request, 'blog/post_detail.html', {'Anuncio': anuncio})
