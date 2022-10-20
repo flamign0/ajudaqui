a28b88447991abb80161f84082637783f845576e
Ruan Massini <79644838+flamign0@users.noreply.github.com>
2022-10-20 10:18:51 -0400

    Delete blog directory

--- blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Anuncio
-
-class AnuncioForm(forms.ModelForm):
-
-    class Meta:
-        model = Anuncio
-        fields = ('autor', 'tipoSv', 'descSv', 'email', 'telefone', 'endereco')
--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('anuncio/<int:pk>/', views.post_detail, name='post_detail'),
-    path('anuncio/new/', views.post_new, name='post_new'),
-    path('anuncio/<int:pk>/edit/', views.post_edit, name='post_edit'),
-]
